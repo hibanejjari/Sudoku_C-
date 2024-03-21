d8a3ea9c072ac426deb617e17f9b33539f43ccf2
hibanejjari <159903043+hibanejjari@users.noreply.github.com>
2024-03-21 02:30:47 +0100

    Add files via upload
    Updated game

---
+++ Sudoku/Sudoku/GameEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class GameEntity // Base class to demonstrate inheritance
+    {
+        protected string Name { get; set; } // Example property
+
+        public GameEntity(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Sudoku/Sudoku/Score.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+
+    public class Score : GameEntity // Inheritance
+    {
+        private int points;
+
+        public Score(string name, int points) : base(name)
+        {
+            this.points = points;
+        }
+
+        // Additional methods here
+    }
+
+}
